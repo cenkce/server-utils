e5a3fb47976ce22bd887f5f141223f92b7dfa52b
cenkce <mail@cenkcetinkaya.com>
2016-08-16 08:53:47 +0300

    update docker

--- ubuntu/install-docker.sh
+++ ubuntu/install-docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# implement from https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04
+# implemented from https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04
 
 sudo apt-get update
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
@@ -7,4 +7,5 @@ echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc
 sudo apt-get update
 apt-cache policy docker-engine
 sudo apt-get install -y docker-engine
+sudo usermod -aG docker $(whoami)
 echo "completed"
